cdfb6d847f0b461c99e7fd007e44bc243d490bf2
mpowloka <mpowloka@listonic.com>
2018-06-29 13:13:19 +0200

    Initial commit

---
+++ app/src/main/java/com/mpowloka/list/BrowseListsActivity.java
@@ -0,0 +1,13 @@
+package com.mpowloka.list;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BrowseListsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_lists);
+    }
+}
